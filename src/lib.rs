7704437e52776095562b55085523473a2c674ca2
Jean-Baptiste Daval <krokmou@crans.org>
2021-04-19 19:26:46 +0200

    Add some lints and warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+#![deny(unsafe_code)]
+
+#![deny(unreachable_patterns)]
+#![warn(missing_copy_implementations)]
+#![warn(missing_docs)]
+#![warn(unreachable_pub)]
+#![warn(unused_lifetimes)]
+#![warn(unused_qualifications)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::inline_always)]
+
+
 pub mod substr;
 pub mod set;
-mod dag;
+pub mod dag;
